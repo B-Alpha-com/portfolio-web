cf6f2ba9531f63f2ed79afe27c474fc50750d1a8
andela <200545410@student.georgianc.on.ca>
2024-03-21 12:39:40 -0400

    Updates

---
+++ precache-manifest.533d2a5905779f00b27bd2bbf77c3836.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9aff79e3d81e7ffb5d6e0f85107b589a",
+    "url": "/portfolio-web/index.html"
+  },
+  {
+    "revision": "e8e8d10112a16c517934",
+    "url": "/portfolio-web/static/css/main.432c2ac5.chunk.css"
+  },
+  {
+    "revision": "22b0d26d36145fbf52cd",
+    "url": "/portfolio-web/static/js/2.2f436571.chunk.js"
+  },
+  {
+    "revision": "2c10491364e9bdf48063",
+    "url": "/portfolio-web/static/js/3.fc5cbf83.chunk.js"
+  },
+  {
+    "revision": "e8e8d10112a16c517934",
+    "url": "/portfolio-web/static/js/main.ec3b6451.chunk.js"
+  },
+  {
+    "revision": "a8c1813fa0eefd97d17f",
+    "url": "/portfolio-web/static/js/runtime~main.7077fc23.js"
+  },
+  {
+    "revision": "526cd39b730e51bec043bf9e2bb77313",
+    "url": "/portfolio-web/static/media/IMG_4667.526cd39b.PNG"
+  },
+  {
+    "revision": "3822d89229d3a5859f5bfc4f9e88e892",
+    "url": "/portfolio-web/static/media/chopify.3822d892.mp4"
+  },
+  {
+    "revision": "8df59aec6af8c8860642b3a017fe12ea",
+    "url": "/portfolio-web/static/media/countdown.8df59aec.mp4"
+  },
+  {
+    "revision": "38fba72cc964bdc21f150180237a028e",
+    "url": "/portfolio-web/static/media/javaWeather.38fba72c.mp4"
+  },
+  {
+    "revision": "54ef27824bf97e5224bac6dabbe6fdfe",
+    "url": "/portfolio-web/static/media/resume.54ef2782.png"
+  }
+]);
